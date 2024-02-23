ca516754d60d408c165fb7b5cfa7a6c0e794d70e
gerhatmilan <gerhat.milan@gmail.com>
2024-02-23 16:08:54 +0100

    Adding database schema diagrams and sql scripts for creating the database structure

---
+++ tools/database/football.sql
@@ -0,0 +1,146 @@
+CREATE SCHEMA "football";
+
+CREATE TABLE "football"."countries" (
+  "country_id" serial PRIMARY KEY,
+  "name" varchar UNIQUE NOT NULL,
+  "flag_link" varchar
+);
+
+CREATE TABLE "football"."leagues" (
+  "league_id" integer PRIMARY KEY,
+  "country_id" integer,
+  "name" varchar UNIQUE NOT NULL,
+  "type" varchar NOT NULL CHECK("football"."leagues"."type" IN ('league', 'cup')),
+  "current_season" integer NOT NULL,
+  "logo_link" varchar,
+  
+  FOREIGN KEY ("country_id")
+    REFERENCES "football"."countries" ("country_id")
+	ON DELETE CASCADE
+);
+
+CREATE TABLE "football"."venues" (
+  "venue_id" integer PRIMARY KEY,
+  "country_id" integer NOT NULL,
+  "city" varchar NOT NULL,
+  "name" varchar UNIQUE NOT NULL,
+  
+  FOREIGN KEY ("country_id")
+    REFERENCES "football"."countries" ("country_id")
+	ON DELETE CASCADE
+);
+
+CREATE TABLE "football"."teams" (
+  "team_id" integer PRIMARY KEY,
+  "country_id" integer NOT NULL,
+  "venue_id" integer,
+  "name" varchar UNIQUE NOT NULL,
+  "short_name" varchar,
+  "logo_link" varchar,
+  "national" boolean NOT NULL DEFAULT false,
+  
+  FOREIGN KEY ("country_id")
+    REFERENCES "football"."countries" ("country_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("venue_id")
+    REFERENCES "football"."venues" ("venue_id")
+	ON DELETE SET NULL
+);
+
+CREATE TABLE "football"."available_seasons" (
+  "league_id" integer,
+  "season" integer,
+  
+  PRIMARY KEY ("league_id", "season"),
+  
+  FOREIGN KEY ("league_id")
+    REFERENCES "football"."leagues" ("league_id")
+	ON DELETE CASCADE
+);
+
+CREATE TABLE "football"."standings" (
+  "league_id" integer,
+  "season" integer,
+  "rank" integer,
+  "team_id" integer,
+  "points" integer NOT NULL,
+  "played" integer NOT NULL,
+  "wins" integer NOT NULL,
+  "draws" integer NOT NULL,
+  "losses" integer NOT NULL,
+  "scored" integer NOT NULL,
+  "condeded" integer NOT NULL,
+  "last_update" timestamp,
+  
+  PRIMARY KEY ("league_id", "season", "rank"),
+  
+  FOREIGN KEY ("league_id", "season")
+    REFERENCES "football"."available_seasons" ("league_id", "season")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("team_id")
+    REFERENCES "football"."teams" ("team_id")
+	ON DELETE SET NULL
+);
+
+CREATE TABLE "football"."players" (
+  "player_id" integer PRIMARY KEY,
+  "team_id" integer,
+  "name" varchar NOT NULL,
+  "age" integer NOT NULL,
+  "number" integer,
+  "position" varchar,
+  "photo_link" varchar,
+  
+  FOREIGN KEY ("team_id")
+    REFERENCES "football"."teams" ("team_id")
+	ON DELETE SET NULL
+);
+
+CREATE TABLE "football"."top_scorers" (
+  "league_id" integer,
+  "season" integer,
+  "rank" integer,
+  "player_id" integer,
+  "played" integer NOT NULL,
+  "goals" integer NOT NULL,
+  "assists" integer NOT NULL,
+  "last_update" timestamp,
+  
+  PRIMARY KEY ("league_id", "season", "rank"),
+  
+  FOREIGN KEY ("league_id", "season")
+    REFERENCES "football"."available_seasons" ("league_id", "season")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("player_id")
+    REFERENCES "football"."players" ("player_id")
+	ON DELETE SET NULL
+);
+
+CREATE TABLE "football"."matches" (
+  "match_id" integer PRIMARY KEY,
+  "date" timestamp,
+  "venue_id" integer,
+  "league_id" integer NOT NULL,
+  "season" integer NOT NULL,
+  "round" integer NOT NULL,
+  "home_team_id" integer NOT NULL,
+  "away_team_id" integer NOT NULL,
+  "status" varchar,
+  "minute" integer,
+  "home_team_goals" integer,
+  "away_team_goals" integer,
+  "last_update" timestamp,
+  
+  FOREIGN KEY ("venue_id")
+    REFERENCES "football"."venues" ("venue_id")
+	ON DELETE SET NULL,
+  FOREIGN KEY ("league_id", "season")
+    REFERENCES "football"."available_seasons" ("league_id", "season")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("home_team_id")
+    REFERENCES "football"."teams" ("team_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("away_team_id")
+    REFERENCES "football"."teams" ("team_id")
+	ON DELETE CASCADE
+);
---
+++ tools/database/users.sql
@@ -0,0 +1,56 @@
+CREATE SCHEMA "users";
+
+CREATE TABLE "users"."users" (
+  "user_id" serial PRIMARY KEY,
+  "email" varchar UNIQUE NOT NULL,
+  "username" varchar UNIQUE NOT NULL,
+  "password" bytea NOT NULL,
+  "registration_date" date NOT NULL
+);
+
+CREATE TABLE "users"."messages" (
+  "message_id" serial PRIMARY KEY,
+  "user_id" integer NOT NULL,
+  "sent_at" timestamp NOT NULL,
+  "message" text,
+  "match_id" integer,
+  "prediction_game_id" integer,
+  
+  FOREIGN KEY ("user_id")
+    REFERENCES "users"."users" ("user_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("match_id")
+    REFERENCES "football"."matches" ("match_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("prediction_game_id")
+    REFERENCES "predictions"."prediction_games" ("prediction_game_id")
+	ON DELETE CASCADE
+);
+
+CREATE TABLE "users"."favorite_leagues" (
+  "user_id" integer NOT NULL,
+  "league_id" integer NOT NULL,
+  
+  PRIMARY KEY ("user_id", "league_id"),
+  
+  FOREIGN KEY ("user_id")
+    REFERENCES "users"."users" ("user_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("league_id")
+    REFERENCES "football"."leagues" ("league_id")
+	ON DELETE CASCADE
+);
+
+CREATE TABLE "users"."favorite_teams" (
+  "user_id" integer NOT NULL,
+  "team_id" integer NOT NULL,
+  
+  PRIMARY KEY ("user_id", "team_id"),
+  
+  FOREIGN KEY ("user_id")
+    REFERENCES "users"."users" ("user_id")
+	ON DELETE CASCADE,
+  FOREIGN KEY ("team_id")
+    REFERENCES "football"."teams" ("team_id")
+	ON DELETE CASCADE
+);
