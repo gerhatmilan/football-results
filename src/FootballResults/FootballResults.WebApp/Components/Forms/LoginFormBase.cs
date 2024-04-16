d793a6b481437bc7b9fe4d3b96989fd5d8a6f10c
gerhatmilan <gerhat.milan@gmail.com>
2024-04-16 02:43:21 +0200

    Resolving an issue where /logout page threw an error due to it being a server static rendered page yet trying to use JSInterop

--- src/FootballResults/FootballResults.WebApp/Components/Forms/LoginFormBase.cs
+++ src/FootballResults/FootballResults.WebApp/Components/Forms/LoginFormBase.cs
@@ -1,17 +1,11 @@
 ﻿using FootballResults.Models.Users;
 using FootballResults.WebApp.Services.Users;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
-using System.Security.Claims;
 
 namespace FootballResults.WebApp.Components.Forms
 {
     public class LoginFormBase : ComponentBase
     {
-        [Parameter]
-        public HttpContext? HttpContext { get; set; }
-
         [Inject]
         protected ILoginService? LoginService { get; set; }
 
@@ -22,6 +16,12 @@ namespace FootballResults.WebApp.Components.Forms
 
         [SupplyParameterFromForm(FormName = "LoginForm")]
         public LoginModel LoginModel { get; set; } = new LoginModel();
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+        }
+
 
         protected async Task AuthenticateUserAsync()
         {
@@ -37,23 +37,8 @@ namespace FootballResults.WebApp.Components.Forms
 
                 if (LoginResult == LoginResult.Success)
                 {
-                    var claims = new List<Claim>
-                    {
-                        new Claim("UserID", userInDatabase!.UserID!.ToString()),
-                        new Claim(ClaimTypes.Name, userInDatabase!.Username!),
-                        new Claim(ClaimTypes.Role, "User")
-                    };
-
-                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-                    await HttpContext!.SignInAsync(claimsPrincipal);
-
-                    NavigationManager!.NavigateTo("/", true);
+                    NavigationManager!.NavigateTo($"/user/login/{userInDatabase!.UserID}", true);
                 }
-            }
-            catch (NavigationException)
-            {
-                NavigationManager!.NavigateTo("/", true);
             }
             catch (Exception)
             {
